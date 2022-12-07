fffc2e0afe8046d36be5996aa408e69262879d97
Joana Tavares <joanalt@ua.pt>
2022-12-07 12:45:29 +0000

    Components

--- src/components/Conteudo.js
+++ src/components/Conteudo.js
@@ -1,9 +1,13 @@
 import React from "react";
 import Header from "./Header";
+import { Titulo } from "./StyledComponents";
 
 function Conteudo() {
   return (
-    <Header />
+    <>
+      <Header />
+      <Titulo>Pickle Rick welcomes you</Titulo>
+    </>
   );
 }
 
--- src/components/StyledComponents.js
+++ src/components/StyledComponents.js
@@ -1,22 +1,30 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 export const Nav = styled.nav`
-background-color: #202329;
-display: flex;
+  background-color: #202329;
+  display: flex;
+`;
+
+export const Logo = styled.img`
+  width: 75px;
 `;
 
 export const Options = styled.ul`
-list-style: none;
-display: flex;
-flex-direction: row;
-align-items: center;
-gap: 5rem;
-a{
+  list-style: none;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 5rem;
+  a {
     text-decoration: none;
-    color: #FFFFFF;
-}
+    color: #ffffff;
+  }
+  a:hover {
+    color: #97ce4c;
+  }
 `;
 
 export const Titulo = styled.h1`
-text-align: center;
+  text-align: center;
+  color: #97ce4c;
 `;
