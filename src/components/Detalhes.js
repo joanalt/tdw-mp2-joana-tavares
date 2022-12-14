34c6cdb9c449acef4475b8e2a9d6e483d029882e
Joana Tavares <joanalt@ua.pt>
2022-12-14 18:55:01 +0000

    details

--- src/components/Detalhes.js
+++ src/components/Detalhes.js
@@ -1,7 +1,14 @@
 import React from "react";
 import Header from "./Header";
 import { useParams, useLocation } from "react-router-dom";
-import { BgDetalhes, Alive, Dead, Unknown, Personagens } from "./Styles.jsx";
+import {
+  Fundo,
+  BgDetalhes,
+  Alive,
+  Dead,
+  Unknown,
+  Personagens,
+} from "./Styles.jsx";
 
 function Detalhes() {
   const { id } = useParams();
@@ -11,46 +18,47 @@ function Detalhes() {
   if (character != null)
     return (
       <>
-        {" "}
         <Header />
-        <Personagens>
-          <BgDetalhes key={id}>
-            <img
-              src={character.data.image}
-              alt={character.data.name}
-              style={{ width: "40%" }}
-            />
-            <div style={{ marginLeft: "30px" }}>
-              <h1>{character.data.name}</h1>
-              <div style={{ display: "flex", alignItems: "center" }}>
-                {character.data.status === "Alive" && <Alive />}
-                {character.data.status === "Dead" && <Dead />}
-                {character.data.status === "unknown" && <Unknown />}
-                <p>{character.data.status}</p>
+        <Fundo>
+          <Personagens>
+            <BgDetalhes key={id}>
+              <img
+                src={character.data.image}
+                alt={character.data.name}
+                style={{ width: "100%" }}
+              />
+              <div style={{ marginLeft: "30px" }}>
+                <h1>{character.data.name}</h1>
+                <div style={{ display: "flex", alignItems: "center" }}>
+                  {character.data.status === "Alive" && <Alive />}
+                  {character.data.status === "Dead" && <Dead />}
+                  {character.data.status === "unknown" && <Unknown />}
+                  <p>{character.data.status}</p>
+                </div>
+                <p>
+                  <b>Species: </b>
+                  {character.data.species}
+                </p>
+                <p>
+                  <b>Type: </b>
+                  {character.data.type}
+                </p>
+                <p>
+                  <b>Gender: </b>
+                  {character.data.gender}
+                </p>
+                <p>
+                  <b>Origin: </b>
+                  {character.data.origin.name}
+                </p>
+                <p>
+                  <b>Last known location: </b>
+                  {character.data.location.name}
+                </p>
               </div>
-              <p>
-                <b>Species: </b>
-                {character.data.species}
-              </p>
-              <p>
-                <b>Type: </b>
-                {character.data.type}
-              </p>
-              <p>
-                <b>Gender: </b>
-                {character.data.gender}
-              </p>
-              <p>
-                <b>Origin: </b>
-                {character.data.origin.name}
-              </p>
-              <p>
-                <b>Last known location: </b>
-                {character.data.location.name}
-              </p>
-            </div>
-          </BgDetalhes>
-        </Personagens>{" "}
+            </BgDetalhes>
+          </Personagens>
+        </Fundo>
       </>
     );
   else return null;
